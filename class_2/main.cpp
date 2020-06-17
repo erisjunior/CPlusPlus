820e3a677c63315871b82cb78387e3b91234a8ed
Antonio Erisvan <erisvan.junior.a@gmail.com>
2020-06-17 08:45:27 -0300

    class_2: finish file

--- class_2/main.cpp
+++ class_2/main.cpp
@@ -1,44 +1,32 @@
 #include <iostream>
 #include <string.h>
 
-void printSuccess()
-{
-  std::cout << "Mensagem adicionada" << std::endl;
-}
+void printSuccess() { std::cout << "Mensagem adicionada" << std::endl; }
 
-void printUseCase()
-{
-  std::cout << "Uso: ./prog add <mensagem>" << std::endl;
-}
+void printUseCase() { std::cout << "Uso: ./prog add <mensagem>" << std::endl; }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
+  if (argc == 1) {
+    printUseCase();
+    return -1;
+  }
+  std::string action = argv[1];
+  // std::string action{argv[1]};
+
+  if (action != "add") {
+    printUseCase();
+    return -1;
+  }
+
   std::string message;
 
-  if (argc == 1)
-  {
-    printUseCase();
-    return -1;
+  if (argc > 2) {
+    message = argv[2];
+  } else {
+    std::cin >> message;
   }
 
-  if (!strcmp(argv[1], "add"))
-  {
-    if (argc > 2)
-    {
-      message = argv[2];
-    }
-    else
-    {
-      std::cin >> message;
-    }
-
-    printSuccess();
-  }
-  else
-  {
-    printUseCase();
-    return -1;
-  }
+  printSuccess();
 
   return 0;
 }
